bfed9171dbd2a6e0b05b3c9d61421cc4b199e5b2
vcdizon04 <vcdizon04@gmail.com>
2019-08-13 16:44:03 +0800

    update

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const mimeType = {
 	console.log('HTTPS Server running on port 443');
 });
 
-httpsServer.createServer(function (req, res) {
+https.createServer(function (req, res) {
 console.log(`${req.method} ${req.url}`);
 
 // parse URL
@@ -85,7 +85,7 @@ fs.exists(pathname, function (exist) {
 
 }).listen(parseInt(port));
   
-console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 var mysql = require('mysql')
 const SocketIOFile = require('socket.io-file');
 const uuidv4 = require('uuid/v4');
@@ -141,6 +141,43 @@ io.sockets.on('connection', function(socket){
         })
 
     // var count = 0;
+	var uploader = new SocketIOFile(socket, {
+		// uploadDir: {			// multiple directories
+		// 	music: 'data/music',
+		// 	document: 'data/document'
+		// },
+		uploadDir: 'data',							// simple directory
+		// accepts: ['audio/mpeg', 'audio/mp3'],		// chrome and some of browsers checking mp3 as 'audio/mp3', not 'audio/mpeg'
+		// maxFileSize: 4194304, 						// 4 MB. default is undefined(no limit)
+		chunkSize: 10240,							// default is 10240(1KB)
+		transmissionDelay: 0,						// delay of each transmission, higher value saves more cpu resources, lower upload speed. default is 0(no delay)
+		overwrite: true, 							// overwrite file if exists, default is true.
+		// rename: function(filename) {
+		// 	var split = filename.split('.');	// split filename by .(extension)
+		// 	var fname = split[0];	// filename without extension
+		// 	var ext = split[1];
+
+		// 	return `${fname}_${ uuidv4() }.${ext}`;
+		// }
+		// rename: 'MyMusic.pdf'
+	});
+	uploader.on('start', (fileInfo) => {
+		console.log('Start uploading');
+		console.log(fileInfo);
+	});
+	uploader.on('stream', (fileInfo) => {
+		console.log(`${fileInfo.wrote} / ${fileInfo.size} byte(s)`);
+	});
+	uploader.on('complete', (fileInfo) => {
+		console.log('Upload Complete.');
+		console.log(fileInfo);
+	});
+	uploader.on('error', (err) => {
+		console.log('Error!', err);
+	});
+	uploader.on('abort', (fileInfo) => {
+		console.log('Aborted: ', fileInfo);
+	});
     // Socket has connected, increase socket count
     socketCount++
     // Let all sockets know how many are connected
